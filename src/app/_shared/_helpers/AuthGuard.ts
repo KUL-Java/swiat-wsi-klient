42783ac322560d4053c07a086d55608bac7fed3f
Damian Michalak <michalak.damian.kul@gmail.com>
2021-01-11 19:17:50 +0100

    Implement new layout for application

--- src/app/_shared/_helpers/AuthGuard.ts
+++ src/app/_shared/_helpers/AuthGuard.ts
@@ -1,18 +1,19 @@
 import {Injectable} from "@angular/core";
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
   constructor(
     private router: Router,
-  ) { }
+  ) {
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user) {
       return true;
     }
-    this.router.navigate(['/home'], { queryParams: { returnUrl: state.url } });
+    this.router.navigate(['/home'], {queryParams: {returnUrl: state.url}});
     return false;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) {
+  }
+
   ngOnInit() {
-  localStorage.removeItem('user');
+    this.router.navigate(['/home']);
   }
 
 }
